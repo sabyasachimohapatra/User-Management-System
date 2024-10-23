b32db7edba25af99966b0dcb1714481921bc6bfe
sabyasachimohapatra <mohapatrasabyasachi162004@gmail.com>
2024-10-23 12:03:27 +0530

    added users route

--- main.js
+++ main.js
@@ -1,46 +1,49 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const mongoose = require('mongoose');
-const multer = require('multer');
+const session = require("express-session");
+const mongoose = require("mongoose");
+const multer = require("multer");
 
 const PORT = process.env.PORT || 3000;
 
 // DB connection
-mongoose.connect(process.env.DB_URL, {
+mongoose
+  .connect(process.env.DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('DB connected');
-}).catch((err) => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: false,
-    saveUninitialized: true
-}));
-app.use((req,res,next)=>{
-    res.locals.message=req.session.message;
-    delete req.session.message;
-    next();
+    saveUninitialized: true,
+  })
+);
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
 });
 
 // set template engine
-app.set('view engine','ejs');
-
+app.set("view engine", "ejs");
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+const router = require('./routes/routes');
+app.use('/',router);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
