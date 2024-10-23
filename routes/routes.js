47ea203bc40cf55759f1d092afde1db92faa555c
sabyasachimohapatra <mohapatrasabyasachi162004@gmail.com>
2024-10-23 12:21:57 +0530

    added ejs Home Page

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const router=express.Router();
 
+router.get('/',(req,res)=>{
+    res.render("index",{title:"Home Page"});
+})
 router.get('/users',(req,res)=>{
     res.send("All Users");
 })
